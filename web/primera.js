86062cb38f31aa635bba65428f170cba1f2e3079
Pablo <olbapgo@gmail.com>
2020-01-18 19:04:42 +0100

    Modal login

--- web/primera.js
+++ web/primera.js
@@ -306,3 +306,16 @@ function pie() {
         }]
     });
 }
+
+$("#btnLogin").click(function(event) {
+
+  //Fetch form to apply custom Bootstrap validation
+  var form = $("#formLogin")
+
+  if (form[0].checkValidity() === false) {
+    event.preventDefault()
+    event.stopPropagation()
+  }
+
+  form.addClass('was-validated');
+});
